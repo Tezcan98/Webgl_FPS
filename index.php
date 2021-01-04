fabeb5e3ec566a438c2670b67580636ccc25568b
tezcan98 <tezcan_enes@hotmail.com>
2021-01-04 20:27:07 +0300

    bitti

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
         <p>Life: <span id="life"></span></p>
     </div>
 
-    <img src="src/aim.png" alt="aim" width="30" height="30" style="position: absolute; z-index: 5;  position: absolute;
+    <img src="src/aim.png" alt="aim" left=20% width="30" height="30" style="position: absolute; z-index: 5;  position: absolute;
     left: 512px; top: 350px; user-select: none;">
     </body>
 
